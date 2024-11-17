ebabd42a8ce971139a71fb829ebd9fd579e69dac
Atlanten94 <00_atlanten.freizeit@icloud.com>
2024-11-17 18:03:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,27 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
+// Speichern der Fortschritte für Checkboxen
+function saveProgress() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const progress = {};
+
+    checkboxes.forEach(checkbox => {
+        progress[checkbox.name] = checkbox.checked;
+    });
+
+    set(ref(database, 'pflegeformular/checkboxes'), progress) // Fix für korrektes Speichern der Checkbox-Daten
+        .then(() => {
+            console.log('Fortschritt erfolgreich gespeichert.');
+        })
+        .catch((error) => {
+            console.error('Fehler beim Speichern des Fortschritts:', error);
+        });
+}
+
 // Laden der Fortschritte für Checkboxen im Spätdienst
 function loadProgress() {
-    const progressRef = ref(database, 'pflegeformularFrüh/checkboxes'); // Fix für den richtigen Pfad
+    const progressRef = ref(database, 'pflegeformular/checkboxes'); // Fix für den richtigen Pfad
     get(progressRef).then((snapshot) => {
         if (snapshot.exists()) {
             const progress = snapshot.val();
@@ -34,24 +52,6 @@ function loadProgress() {
     });
 }
 
-// Speichern der Fortschritte für Checkboxen
-function saveProgress() {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const progress = {};
-
-    checkboxes.forEach(checkbox => {
-        progress[checkbox.name] = checkbox.checked;
-    });
-
-    set(ref(database, 'pflegeformularFrüh/checkboxes'), progress) // Fix für korrektes Speichern der Checkbox-Daten
-        .then(() => {
-            console.log('Fortschritt erfolgreich gespeichert.');
-        })
-        .catch((error) => {
-            console.error('Fehler beim Speichern des Fortschritts:', error);
-        });
-}
-
 // Zurücksetzen der Checkboxen Nachtdienst
 function resetCheckboxes() {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -59,7 +59,7 @@ function resetCheckboxes() {
         checkbox.checked = false;
     })
     
-    remove(ref(database, 'pflegeformularFrüh/checkboxes'))
+    remove(ref(database, 'pflegeformular/checkboxes'))
         .then(() => {
             alert('Kontrollkästchen erfolgreich zurückgesetzt!');
         })
@@ -79,10 +79,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Reset-Button
-    document.getElementById('resetBtnFr').addEventListener('click', () => {
+    document.getElementById('resetBtn').addEventListener('click', () => {
         resetCheckboxes();
     });
 
+    // Speichern der Fortschritte und Text-Inputs (falls nötig)
+    document.getElementById('saveBtn').addEventListener('click', () => {
+        saveProgress();
+    });
 });
 
 
