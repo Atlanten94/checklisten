07cb6f372ec26b61d5eb66776322eab9ebbea239
Atlanten94 <00_atlanten.freizeit@icloud.com>
2024-11-17 05:33:31 +0100

    Update nacht.js

--- nacht.js
+++ nacht.js
@@ -1,4 +1,23 @@
-    // Speichern der Fortschritte für Checkboxen
+// Your web app's Firebase configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase, ref, set, get, remove } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB3a7mBkYun-pW-reZ90qDzWKxM-Nntbc0",
+    authDomain: "dienstaufgaben-42703.firebaseapp.com",
+    databaseURL: "https://dienstaufgaben-42703-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "dienstaufgaben-42703",
+    storageBucket: "dienstaufgaben-42703.appspot.com",
+    messagingSenderId: "308022795905",
+    appId: "1:308022795905:web:8d395d93530aaf368ef780",
+    measurementId: "G-WDLP80WGEB"
+};
+
+// Firebase-Konfiguration und Initialisierung
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// Speichern der Fortschritte für Checkboxen
 function saveProgress() {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     const progress = {};
