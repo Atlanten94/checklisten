affe92b1344defb940cac4dda1fb2a234fb6b2e3
Atlanten94 <00_atlanten.freizeit@icloud.com>
2024-11-17 16:47:11 +0100

    Update authGuard.js

--- authGuard.js
+++ authGuard.js
@@ -1,4 +1,5 @@
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { auth } from './script.js';
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const auth = getAuth();
 
