512038ff04599eb7bf914587416ebf9ac1b1ae20
Atlanten94 <00_atlanten.freizeit@icloud.com>
2024-11-17 01:13:36 +0100

    Update frühdienst.js

--- frühdienst.js
+++ frühdienst.js
@@ -1,3 +1,6 @@
+
+//__________________________________________A U F N A H M E D I E N S T _________________________________________
+
 document.getElementById('aufnahme_1').addEventListener('click', function() {
     var panelBody = document.getElementById('aufgaben-Aufnahmedienst-panel-body1');
     var panelIcon = document.getElementById('aufgaben-Aufnahmedienst-panel-icon1');
@@ -35,6 +38,65 @@ document.getElementById('aufnahme_2').addEventListener('click', function() {
         panelIcon.style.transform = 'rotate(180deg)';
     }
 });
+
+document.getElementById('aufnahme_3').addEventListener('click', function() {
+    var panelBody = document.getElementById('aufgaben-Aufnahmedienst-panel-body3');
+    var panelIcon = document.getElementById('aufgaben-Aufnahmedienst-panel-icon3');
+    
+    // Toggle display of the panel body
+    if (panelBody.style.display === 'none' || panelBody.style.display === '') {
+        panelBody.style.display = 'block';
+    } else {
+        panelBody.style.display = 'none';
+    }
+    
+    // Toggle rotation of the icon
+    if (panelIcon.style.transform === 'rotate(180deg)') {
+        panelIcon.style.transform = 'rotate(0deg)';
+    } else {
+        panelIcon.style.transform = 'rotate(180deg)';
+    }
+});
+
+document.getElementById('aufnahme_4').addEventListener('click', function() {
+    var panelBody = document.getElementById('aufgaben-Aufnahmedienst-panel-body4');
+    var panelIcon = document.getElementById('aufgaben-Aufnahmedienst-panel-icon4');
+    
+    // Toggle display of the panel body
+    if (panelBody.style.display === 'none' || panelBody.style.display === '') {
+        panelBody.style.display = 'block';
+    } else {
+        panelBody.style.display = 'none';
+    }
+    
+    // Toggle rotation of the icon
+    if (panelIcon.style.transform === 'rotate(180deg)') {
+        panelIcon.style.transform = 'rotate(0deg)';
+    } else {
+        panelIcon.style.transform = 'rotate(180deg)';
+    }
+});
+
+document.getElementById('aufnahme_5').addEventListener('click', function() {
+    var panelBody = document.getElementById('aufgaben-Aufnahmedienst-panel-body5');
+    var panelIcon = document.getElementById('aufgaben-Aufnahmedienst-panel-icon5');
+    
+    // Toggle display of the panel body
+    if (panelBody.style.display === 'none' || panelBody.style.display === '') {
+        panelBody.style.display = 'block';
+    } else {
+        panelBody.style.display = 'none';
+    }
+    
+    // Toggle rotation of the icon
+    if (panelIcon.style.transform === 'rotate(180deg)') {
+        panelIcon.style.transform = 'rotate(0deg)';
+    } else {
+        panelIcon.style.transform = 'rotate(180deg)';
+    }
+});
+
+//-----------------------------------------------------------------------------------
 
 document.getElementById('aufgaben-Basisdienst').addEventListener('click', function() {
     var panelBody = document.getElementById('aufgaben-Basisdienst-panel-body');
