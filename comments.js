21be106301e2cbcdc9fbc59c996e9751c8f2ab56
Atlanten94 <00_atlanten.freizeit@icloud.com>
2024-11-18 01:27:49 +0100

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,21 +1,6 @@
 // Your web app's Firebase configuration
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
-
-// Verwende deine bestehende Firebase-Konfiguration
-const firebaseConfig = {
-    apiKey: "AIzaSyB3a7mBkYun-pW-reZ90qDzWKxM-Nntbc0",
-    authDomain: "dienstaufgaben-42703.firebaseapp.com",
-    databaseURL: "https://dienstaufgaben-42703-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "dienstaufgaben-42703",
-    storageBucket: "dienstaufgaben-42703.appspot.com",
-    messagingSenderId: "308022795905",
-    appId: "1:308022795905:web:8d395d93530aaf368ef780",
-    measurementId: "G-WDLP80WGEB"
-};
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+import { checkAuthentication, logout, database } from './auth.js';
+import { ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
 const commentForm = document.getElementById('commentForm');
 const commentInput = document.getElementById('commentInput');
