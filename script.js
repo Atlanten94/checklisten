45164c4a4a5766c6df8e5ed03af2989ab516131c
Atlanten94 <00_atlanten.freizeit@icloud.com>
2024-11-18 04:21:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { checkAuthentication, database, ref, set, get, remove } from './auth.js';
 import { logout } from './auth.js';
+import { updatePassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 // Laden der Fortschritte für Checkboxen und Textinputs
 function loadProgress() {
